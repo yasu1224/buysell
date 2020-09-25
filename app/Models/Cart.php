6e48d43bead6d2c5fc6749a1ca90c4688d7be592
yasu1224 <yasu1224x@gmail.com>
2020-09-25 17:54:04 +0900

    cart.php更新

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -57,4 +57,12 @@ class Cart extends Model
         }
         return $message;
     }
+    public function checkoutCart()
+    {
+        $user_id = Auth::id();
+        $checkout_items = $this->where('user_id', $user_id)->get();
+        $this->where('user_id', $user_id)->delete();
+
+        return $checkout_items;
+    }
 }

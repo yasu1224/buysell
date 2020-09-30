72a3c0e4f39a7543540fbedeccfcef87302f65ac
yasu1224 <yasu1224x@gmail.com>
2020-09-30 15:50:10 +0900

    不要ファイル削除

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,45 @@
 
 Route::get('/', 'ShopController@index');
 
-Route::group(['middleware' => ['auth']], function(){
-    Route::get('/mycart', 'ShopController@myCart');
-    Route::post('/mycart', 'ShopController@addMycart');
-    Route::post('/cartdelete', 'ShopController@deleteCart');
-    Route::post('/checkout', 'ShopController@checkout');
+
+// ユーザー
+Route::namespace('User')->prefix('user')->name('user.')->group(function(){
+    
+    //ログイン認証関連
+    Auth::routes([
+        'register' => true,
+        'reset'    => false,
+        'verify'   =>false,
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:user')->group(function(){
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+    });
 });
+
+// 管理者
+Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+
+    Auth::routes([
+        'register' => true,
+        'reset'    => false,
+        'verify'   =>false,
+    ]);
+
+    // ログイン認証後
+    Route::middleware('auth:admin')->group(function(){
+        // TOPページ
+        Route::resource('home', 'HomeController', ['only' => 'index']);
+    });
+});
+
+// Route::group(['middleware' => ['auth']], function(){
+//     Route::get('/mycart', 'ShopController@myCart');
+//     Route::post('/mycart', 'ShopController@addMycart');
+//     Route::post('/cartdelete', 'ShopController@deleteCart');
+//     Route::post('/checkout', 'ShopController@checkout');
+// });
 
 Auth::routes();

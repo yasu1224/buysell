e9cc0cf7ee84fd94bcd544606714f7a1faa91ec8
yasu1224 <yasu1224x@gmail.com>
2020-10-05 16:35:04 +0900

    indexメソッド定義
    クエリビルダ
    use

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Stock;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -13,9 +15,20 @@ class HomeController extends Controller
         $this->middleware('auth:admin');
     }
 
-    public function index()
+    public function home()
     {
         return view('admin.home');
+    }
+
+    public function index()
+    {
+        $query = DB::table('stocks');
+        $query->select('imgpath', 'id', 'name', 'detail', 'fee', 'created_at');
+        $query->orderBy('created_at', 'asc');
+        $stocks = $query->paginate(20);
+
+        // dd($contacts);
+        return view('admin.index',  compact('stocks'));
     }
 
 }

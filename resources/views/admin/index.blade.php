f215250530a4c153e6b2047e4a71d18c4a51ab0c
yasu1224 <yasu1224x@gmail.com>
2020-11-26 17:03:48 +0900

    商品登録ページのページネーション

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -46,6 +46,7 @@
                       @endforeach
                       </tbody>
                     </table>
+                    {{ $stocks->links() }}
                 </div>
             </div>
         </div>
